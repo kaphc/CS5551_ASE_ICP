60fd1b0a08c7e46a3f685855f4a7d248b3a67a65
kaphc <kaphc@mail.umkc.edu>
2018-08-24 20:24:46 -0500

    Add ICP-1 source and docs

---
+++ ICP-1/Source/install.ps1
@@ -0,0 +1,15 @@
+#All softwares are managed by Chocolatey - package manager for Windows
+#install git
+choco install -y git
+#install github desktop
+choco install -y github
+#install webstorm
+choco install -y webstorm
+#install android studio
+choco install -y androidstudio
+#install brackets
+choco install -y brackets
+#install nodejs
+choco install -y nodejs
+#install ionic
+npm install -g cordova ionic
---
+++ ICP-1/Source/verify.ps1
@@ -0,0 +1,8 @@
+#git verification
+git --version
+git config --global user.email
+#nodejs verification
+npm --version
+#ionic cli verification
+ionic --version
+ionic
