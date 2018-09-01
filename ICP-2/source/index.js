88901bd207748762131a798fbec380327f5a4788
kaphc <kaphc@mail.umkc.edu>
2018-08-31 21:52:48 -0500

    Formatting code

--- ICP-2/source/index.js
+++ ICP-2/source/index.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         componentRestrictions: {country: "usa"}
     };
     let location = document.getElementById("location");
+    // feature to add auto complete for input field using google maps API
     new google.maps.places.Autocomplete(location, options);
 });
 
@@ -11,9 +12,10 @@ angular.module('weather', [])
     .controller('weatherctrl', function ($scope, $http) {
         $scope.getWeather = function () {
             let location = document.getElementById("location").value;
+            // filter location using regex to generate valid parameters for the API call
             let filtered_location = location.replace(/(.*)(, USA)/, '$1');
             let table = document.getElementById("weather-table");
-            // table.innerHTML = "";
+            // API call using get method
             $http.get('http://api.wunderground.com/api/4bbbc25f4f5946dd/hourly/q/' + filtered_location + '/.json')
                 .then(function (response) {
                     for (let res = 0; res < 5; res++) {
@@ -24,6 +26,7 @@ angular.module('weather', [])
                             response.data.hourly_forecast[res].condition,
                             icon_url
                         );
+                        // append rows to table
                         let row = table.insertRow(res + 1);
                         for (let col = 0; col < weather_result.length; col++) {
                             row.insertCell(col).innerHTML = weather_result[col];
